65b2c59aa01cf24cf8337c016a47211fa2c24352
Lal krishna N C <lalukrishnavallil01@gmail.com>
2021-10-14 16:53:12 +0530

    git signin with one bug

--- src/components/views/signin.js
+++ src/components/views/signin.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React,{useState} from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
 const Signin=()=>{
-    return(
-        <div>
-             <div class="card">
-        <div class="card-image">
-        <div class="coloumn">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">email</i>
-          <input id="icon_telephone" type="tel" class="validate"></input>
-          <label for="icon_telephone">Email</label>
-        </div>
-        <div class="input-field col s6">
-          <i class="material-icons prefix">password</i>
-          <input id="icon_telephone" type="tel" class="validate"></input>
-          <label for="icon_telephone">Password</label>
-        </div>
+  const history = useHistory();
+ 
+  const [password, setPassword] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const postData = () => {
+   
+     fetch("/signup", {
+       method: "post",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify({
+         
+         password,
+         email,
+       }),
+     })
+       .then((res) => res.json())
+       .then((data) => {
+         if (data.error) M.toast({ html: data.error, classes: "#f44336 red" });
+         else {
+           M.toast({ html: data.message, classes: "#4caf50 green" });
+           history.push("/");
+         }
+       })
+       .catch((err) => {
+         console.log(err);
+       });
+     
+   };
+return(
+ 
+ <div className="mycard">
+ <div className="card auth_card">
+ <div class="coloumn">
+
+     
+     <div class="input-field col s6">
+       <i class="material-icons prefix">email</i>
+       <input id="icon_telephone" type="tel" class="validate" value={email} onChange={(e)=>setEmail(e.target.value)} ></input>
+       <label for="icon_telephone">Email</label>
+     </div>
+     <div class="input-field col s6">
+       <i class="material-icons prefix">password</i>
+       <input id="icon_telephone" type="tel" class="validate" class="validate" value={password} onChange={(e)=>setPassword(e.target.value)} ></input>
+       <label for="icon_telephone">Password</label>
+       <button className="btn #2196f3 blue" onClick={()=> postData()}>Login</button>
+         <h5> <Link to ="signup">Don't have have an account </Link>{ " "} </h5>
+     </div>
    </div>
    </div>
    </div>
-   </div>
-    )
+)
 }
 export default Signin;
